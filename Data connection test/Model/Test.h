b260a102dc2294fba109b5c4097e14dd3e16996a
hunar <hunur.khanna@gmail.com>
2013-06-26 22:19:59 +0800

    Initial commit

---
+++ Data connection test/Model/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Data connection test
+//
+//  Created by Hunar Khanna on 26/6/13.
+//  Copyright (c) 2013 NUS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Data connection test/Model/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Data connection test
+//
+//  Created by Hunar Khanna on 26/6/13.
+//  Copyright (c) 2013 NUS. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
